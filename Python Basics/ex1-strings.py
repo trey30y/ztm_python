166f8adaca04c051beb34b7bd059502792cfa1d8
traian <traian30y@yahoo.co.uk>
2025-07-25 20:19:15 +0100

    ex1 formatted strings

---
+++ Python Basics/ex1-strings.py
@@ -0,0 +1,5 @@
+name = "Johnny"
+age = 30
+
+print("Hello, " + name + ". You are " + str(age) + " years old")  # Concatenation
+print(f"Hello, {name}. You are {age} years old")  # f-string formatting
